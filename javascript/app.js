d1ea044f5308f1a4e105e1b0d7eec74d1de3115e
Naseeb-ullah356 <160338963+Naseeb-ullah356@users.noreply.github.com>
2025-04-30 17:12:24 +0500

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,142 @@
+// alert('helllo')
+// var ans =  confirm("are you adult?")
+
+// console.log(ans)
+//    var  ans = prompt("enter your name ")
+//    console.log(ans) var 
+
+// var nam = prompt("enter your name ")
+// console.log("user name is", nam)
+// data types 
+// premitive 
+// number ,string , bolean , undefined, null symboll,, nan
+
+//  if else coonditional 
+// if (10>19) {
+//  console.log("correct")    
+// } else{n 
+//     console.log("not correct")
+// }
+//   let a = 10
+//   if(a>11){
+//     console.log("hello ")
+//   }else{
+//     console.log("hii")
+//   }
+//  let age =   prompt("enter your age")
+//  if (age>18){
+//     console.log("you can vote")
+//  }else{
+//     console.log("you  caN NOT ")
+//  }
+// loops   
+//   for(let i = 0; i<5; i++){
+//     console.log(i)
+//   }
+// do while loop
+//   let  counter = 1; 
+//   do{
+//     console.log(counter)
+//     counter++
+//   } while(counter<=5)
+    // let counter = 1 ;
+    // do{
+    //     console.log(counter)
+    //     counter++
+
+    // } while(counter<=100)
+    // const a = [1,2,3,4,5,6,7,8,9]
+    // for (let i = 0; i<a.length; i++){
+    //     console.log(a[i])
+    // } 
+    // const a = [1,2,3,4,5,6]
+    // for(let i = 0; i<a;  i++){
+    //     console.log(a[i])
+    // }
+
+    // var a = 10;
+    // console.log(a)
+    //   var age = prompt("enter your age");
+    //   if (age>18){
+    //     console.log("you can vote")
+    //   } else{
+    //     console.log("ypu can't")
+    //   }
+    // functions
+    //  function walk(){
+    //     console.log("good morning")
+    //  }
+    //   walk()
+    //  function greet(a){
+    //     console.log("hello everyone",a)
+    // }
+    //  greet(100)
+    //  function abc(){
+    //     console.log("hello")
+    //      return 20
+    //  }
+    //      var  a = abc();
+    //      console.log(a)
+     
+     //  for each 
+    //   var arr = [ 1,2,3,4,5,6]
+    //    arr.forEach(function(elem){
+    //     console.log("hello", elem)
+    //    })
+    //  var obj = {
+    //     n : "1",
+    //     a : "2",
+    //     s : "3"
+
+    //  }
+    //   console.log(obj.n)
+    //  var user = {
+    //     model:"iphone",
+    //     price: 10,
+    //     color: "white"
+    //  }
+    //   var user2 = {
+    //     model:"nokia",
+    //     price: 30,
+    //     color: "black"
+    //   }
+    //    console.log(user.color)
+    //  var user = {
+    //    userName : "naseeb", 
+    //    greet : function(){
+    //     console.log("hello")
+    //      return ("hii")
+    //    }
+    //  }
+    //   console.log(user. greet())
+      /// array of objects
+      //  var arr = [
+      //   {name : 'NASEEEB',
+      //    age : 23,
+      //    },
+      //    {
+      //     name : "irfan",
+      //     age : 20
+      //    }
+      //  ]
+      //   console.log(arr[1].name)
+        // Dom 
+        // //  var a = document.querySelector("h1");
+        //  console.log(a);
+          
+         // change html through dom
+          // var h1 = document.querySelector("h1")
+          // h1.innerHTML= "sahi se padlo java script"
+             // change css through dom
+              // var h1 = document.querySelector("h1")
+              //  h1.innerHTML= " chnage krdia hai";
+              //  h1.style.color = "red";
+              //  h1.style.backgroundColor = "black";
+              // events in dom
+               
+              // var h1 = document.querySelector("h1")
+              //  h1.addEventListener("mouseenter", function(){
+              //   // console.log("hello")
+              //    h1.innerHTML = "changed";
+              //    h1.style.color =" red"
+              //  })
